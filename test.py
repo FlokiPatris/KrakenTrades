eec3bee22a600aefe7690943916edecb49ae5cc6
Floki Patris <f.kotras@gmail.com>
2025-08-14 23:34:23 +0200

    Bandits comming.

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-# tests/trigger.txt
-AWS_SECRET_ACCESS_KEY = "AKIA1234567890ABCDEF"
+# bandit_trigger.py
+import subprocess
+import hashlib
+
+# Insecure: shell=True
+subprocess.call("ls -l", shell=True)  # nosec
+
+# Weak hash
+hashlib.md5(b"test").hexdigest()
+
+# Dangerous eval
+eval("2 + 2")
