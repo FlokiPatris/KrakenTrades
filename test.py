470da0d992e447a5e146291d3fe8b5a5873720ad
Floki Patris <f.kotras@gmail.com>
2025-08-14 23:44:21 +0200

    Trigger git leaks once again.

--- test.py
+++ test.py
@@ -10,3 +10,8 @@ hashlib.md5(b"test").hexdigest()
 
 # Dangerous eval
 eval("2 + 2")
+
+
+AWS_SECRET_ACCESS_KEY = "AKIA1234567890ABCDEF"
+API_KEY = "1234567890abcdef"
+SYS_ADMIN_PASSWORD = "SuperSecretPasswordY123"
