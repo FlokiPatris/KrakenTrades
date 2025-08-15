3b35a798bbedcf8867972700f9328d0516253ff2
Floki Patris <f.kotras@gmail.com>
2025-08-15 22:02:04 +0200

    Now Trivy check PR should pass

--- secretstrigger.py
+++
@@ -1,7 +0,0 @@
-# fake-secrets.txt
-# This is a TEST FILE to trigger git-secrets — DO NOT use in production
-AWS_ACCESS_KEY_ID = AKIAFAKEFAKEFAKE1234
-AWS_SECRET_ACCESS_KEY = wJalrXUtnFEMI / K7MDENG / bPxRfiCYFAKEKEY1234
-
-# Random "password" patterns that may also match common detectors
-db_password = "SuperSecret123!"
