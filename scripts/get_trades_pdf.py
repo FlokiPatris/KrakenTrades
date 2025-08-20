b4f910021afd3c118df6145db3003549ebdffe4e
Floki Patris <f.kotras@gmail.com>
2025-08-20 05:10:16 +0200

    refined get_trades_pdf

--- scripts/get_trades_pdf.py
+++ scripts/get_trades_pdf.py
@@ -2,12 +2,21 @@ import os
 import json
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
 
-creds_json = json.loads(os.environ["GOOGLE_DRIVE_JSON_KEY"])
+# Load JSON key from environment
+creds_json = json.loads(os.environ['GOOGLE_DRIVE_JSON_KEY'])
+
+# Setup GoogleAuth with service account
 gauth = GoogleAuth()
-gauth.credentials = gauth.credentials_from_service_account_info(creds_json)
+scope = ['https://www.googleapis.com/auth/drive']
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(creds_json, scope)
+gauth.credentials = credentials
+
+# Connect to Google Drive
 drive = GoogleDrive(gauth)
 
-file_id = "1qWAkAYZTY3oZK-eK41F4Psuhdm4cAUub"
-file = drive.CreateFile({"id": file_id})
-file.GetContentFile("trades.pdf")
+# Download file
+file_id = '1qWAkAYZTY3oZK-eK41F4Psuhdm4cAUub'
+file = drive.CreateFile({'id': file_id})
+file.GetContentFile('trades.pdf')
