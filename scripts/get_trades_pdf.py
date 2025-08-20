9d5f4b83f94844d7fc6df1aff01e3272041a4d89
Floki Patris <f.kotras@gmail.com>
2025-08-20 04:56:08 +0200

    Added google JSON processing

---
+++ scripts/get_trades_pdf.py
@@ -0,0 +1,13 @@
+import os
+import json
+from pydrive2.auth import GoogleAuth
+from pydrpyive2.drive import GoogleDrive
+
+creds_json = json.loads(os.environ["GOOGLE_DRIVE_JSON_KEY"])
+gauth = GoogleAuth()
+gauth.credentials = gauth.credentials_from_service_account_info(creds_json)
+drive = GoogleDrive(gauth)
+
+file_id = "1qWAkAYZTY3oZK-eK41F4Psuhdm4cAUub"
+file = drive.CreateFile({"id": file_id})
+file.GetContentFile("trades.pdf")
