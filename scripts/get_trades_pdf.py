07a2d70e8f048018d7bcd1b5d8f5745d4bc1f4ef
FilipKotras <54849299+FlokiPatris@users.noreply.github.com>
2025-12-07 20:49:44 +0100

    Update file ID for downloading trades PDF

--- scripts/get_trades_pdf.py
+++ scripts/get_trades_pdf.py
@@ -90,7 +90,7 @@ def main() -> None:
         drive = setup_drive(creds_json)
 
         # Download the target file
-        file_id = "1W5fzVRmSSgwgiCjPP1F4MNDBk1MYi6dq"
+        file_id = "1pSxpuDO5vwwbusFMvPKAqoE0rRfFjvCM"
         download_file(drive, file_id, PathsConfig.KRAKEN_TRADES_PDF)
 
     except Exception as exc:
